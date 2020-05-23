b01897edcd589dc33fe8aec68db2c599f3497daf
asenaasahin <tugba.sahin@ug.bilkent.edu.tr>
2020-05-24 01:46:54 +0300

    Information Java class for notificaions

--- app/src/main/java/com/example/android/bee/Information.java
+++ app/src/main/java/com/example/android/bee/Information.java
@@ -1,12 +1,45 @@
 package com.example.android.bee;
 
 public class Information {
-    private int[][] information;
+    private String[] information = {"Today we’re going to have an awesome day filled with positive experience.",
+            "Throughout the day, keep in mind how you could make today 5% better than yesterday.",
+            "Don’t let the world change your smile. Let your smile change the world.",
+            "I’m ready ready for an awesome day.",
+            "My life isn't perfect but I am blessed with privileges that I am grateful for.",
+            "Track gratitude and achievement with a journal. Include 3 things you were grateful for and 3 things you were able to accomplish each day.",
+            "Do something today that your future self will thank you for.",
+            "Sometimes we’re tested not to show our weaknesses, but to discover our strengths.",
+            "Never regret a day in your life. Good days give you happiness and bad days give you experience.",
+            "Acknowledging the good that you already have in your life is the foundation for all abundance.",
+            "You’re just one journal entry away from an improved mood!",
+            "Optimism may help you live longer :)",
+            "Bananas can help improve your mood :)",
+            "Exercise will give you more energy, even when you’re tired. :)",
+            "Laughing is good for the heart and can increase blood flow by 20 percent.",
+            "Feeling stressed? Read. Getting lost in a book can lower levels of cortisol, or other unhealthy stress hormones, by 67 percent.",
+            "Drinking coffee can reduce the risk of depression, especially in women.",
+            "Chewing gum makes you more alert, relieves stress and reduces anxiety levels.",
+            "Yoga can boost your cognitive function and lowers stress.",
+            "Walking outside – or spending time in green space – can reduce negative thoughts and boost self-esteem.",
+            "Make a plan for attaining goals that you believe will make you happy.",
+            "Surround yourself with happy people.",
+            "Spend a few minutes each day thinking about the things that make you happy.",
+            "Take a minute, sit down and just reflect on what you appreciate and love about your partner.Or a friend or family member. Or yourself.",
+            "Say yes to something new.",
+            "Learn to forgive and forget.",
+            "Express gratitude for what you have.",
+            "Be more optimistic about the future and your ability to accomplish life goals.",
+            "Get enough sleep.",
+            "Dance around while you do your housework. Not only will you get chores done, but dancing reduces levels of cortisol (the stress hormone), and increases endorphins.",
+            "Chocolate is good for your skin; its antioxidants improve blood flow and protect against UV damage.",
+            "Eating oatmeal provides a serotonin boost to calm the brain and improve your mood.",
+            "Drinking water helps you look and feel your best.",
+            "Start your day with a cup of coffee. Coffee consumption is linked to lower rates of depression. If you can’t drink coffee because of the caffeine, try another good-for-you drink like green tea."};
 
     public Information() {
     }
 
-    public int[][] getInformation() {
+    public String[] getInformation() {
         return information;
     }
 }
